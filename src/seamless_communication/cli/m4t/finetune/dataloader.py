b6a7a15853149514637fdd85f63bdc9e107b1c28
Erik Beltran <ebellob@upv.es>
2025-03-01 11:09:18 +0100

    fixed save

--- src/seamless_communication/cli/m4t/finetune/dataloader.py
+++ src/seamless_communication/cli/m4t/finetune/dataloader.py
@@ -343,7 +343,7 @@ class UnitYDataLoader:
             sample.source.waveform = torch.tensor(cvss_sample['audio']['array'])
 
         
-        saved_sample = samples[0].source.waveform[0].unsqueeze(0).cpu().float(),
+        saved_sample = samples[0].source.waveform.unsqueeze(0).cpu().float()[0],
         with open('dataloader_log.txt', mode='w+') as fl:
             
             print(f"Saved sample: {saved_sample}", file=fl)
