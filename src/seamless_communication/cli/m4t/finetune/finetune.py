86330e1afa57a73441985df05584da67d2b86273
Erik Beltran <ebellob@upv.es>
2025-03-13 09:56:37 +0100

    fix for tensor item ids

--- src/seamless_communication/cli/m4t/finetune/finetune.py
+++ src/seamless_communication/cli/m4t/finetune/finetune.py
@@ -281,7 +281,7 @@ def main() -> None:
     cvss_src_train_dataset, cvss_src_eval_dataset = None, None
 
     if args.is_source_cvss:
-        cvss_src_train_dataset = load_from_disk(args.src_path)['train']
+        cvss_src_train_dataset = load_from_disk(args.src_path)['train'].cast_column('id', int)
         # cvss_ES_train_dataset = load_dataset(
         #     'ebellob/cvss-c-fleurs-format-source',
         #     split='train'
@@ -292,7 +292,7 @@ def main() -> None:
         #     'ebellob/cvss-c-fleurs-format-source',
         #     split='validation'
         # )
-        cvss_src_eval_dataset = load_from_disk(args.src_path)['validation']
+        cvss_src_eval_dataset = load_from_disk(args.src_path)['validation'].cast_column('id', int)
 
 
     # TODO: delete unused params to reduce GPU memory consumption
